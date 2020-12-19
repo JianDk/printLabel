59f304618915bd20d2395ae66001d1bba669595d
JianWu <jian_dfh@hotmail.com>
2020-12-19 20:13:00 +0100

    udpate

--- dimsumBoxLabel.py
+++ dimsumBoxLabel.py
@@ -1,18 +1,14 @@
-#! /usr/bin/python
 import os
 from PIL import Image
 import zpl
 
-label = zpl.Label(160,40)
+label = zpl.Label(220,100)
 label.origin(35,20)
 label.set_darkness(30)
-label.write_graphic(Image.open('label.png'), width = 38)
+label.write_graphic(Image.open('FamilyBox.png'), width = 38)
 label.endorigin()
 printStr = bytes(label.dumpZPL(), encoding='utf8')
 
 text_file = open("printLabel.zpl", "wb")
 text_file.write(printStr)
 text_file.close()
-
-for i in range(50):
-    os.system("lp -o raw printLabel.zpl")
